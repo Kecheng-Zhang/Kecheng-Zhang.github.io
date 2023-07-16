c8628be0e74dd088f681bb8b9256d41d3ddf78fb
Kecheng Zhang <kechengzhang28@gmail.com>
2023-07-17 00:42:39 +1000

    add tag search

---
+++ assets/js/filter.js
@@ -0,0 +1,43 @@
+
+let tags = document.getElementById("tags").querySelectorAll("div");
+
+// show the posts
+function showPosts(id) {
+    document.getElementById(id).style.display = "block";
+}
+
+// hide the posts
+function hidePosts(id) {
+    document.getElementById(id).style.display = "none";
+}
+
+// show all posts with the given tag
+function show(tag) {
+    hidePosts("posts-all");
+    for (i=0; i<tags.length; i++) {
+        if (tags[i].id != tag) {
+            hidePosts("posts-".concat(tags[i].id));
+        } else {
+            showPosts("posts-".concat(tags[i].id));
+        }
+    }
+}
+
+// show "all posts" section but hide others
+function reset() {
+    showPosts("posts-all");
+    for (i=0; i<tags.length; i++) {
+        hidePosts("posts-".concat(tags[i].id));
+    }
+}
+
+show_all = document.getElementById("show-all");
+show_all.addEventListener("click", reset);
+
+reset();
+
+for (i=0; i<tags.length; i++) {
+    tags[i].addEventListener("click", function() {
+        show(this.id)
+    });
+}
